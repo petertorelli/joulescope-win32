314dc533a3cd513e3d9fba314352848a35ef7401
Peter Torelli <peter.j.torelli@gmail.com>
2021-01-14 15:55:47 -0800

    Updated to '6b92e38'; added (c) acknowledgements

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
+/**
+ * Copyright 2021 Peter Torelli
+ * Copyright 2020 Jetperch LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "main.hpp"
-#define VERSION "0.15.0"
-#define PYJOULESCOPE_GITHUB_HEAD "3a6ff5f"
+// Note: Only the device and raw_processor are critical.
+#define PYJOULESCOPE_GITHUB_HEAD "6b92e38"
+#define VERSION "1.0.0"
 
 /**
  * TODO
